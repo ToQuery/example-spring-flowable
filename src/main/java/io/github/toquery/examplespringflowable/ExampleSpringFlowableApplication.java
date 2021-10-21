a116e6c5f1b24a57cee9ab011dee54a6f4067b75
ToQuery <toquery@qq.com>
2021-10-21 13:15:31 +0800

    init

---
+++ src/main/java/io/github/toquery/examplespringflowable/ExampleSpringFlowableApplication.java
@@ -0,0 +1,13 @@
+package io.github.toquery.examplespringflowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringFlowableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringFlowableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/toquery/examplespringflowable/ExampleSpringFlowableApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.toquery.examplespringflowable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleSpringFlowableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
